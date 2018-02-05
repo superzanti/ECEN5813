685295e8155c3a982de1c80fd052d05151aac875
“Seth <miers.seth@gmail.com>
2018-02-04 17:32:58 -0700

    adding comments

--- project1/memory.c
+++ project1/memory.c
@@ -1,18 +1,32 @@
 #include "memory.h"
 
+extern void __HeapBase, __HeapLimit;
+
 uint8_t * my_memmove(uint8_t * src, uint8_t * dst, size_t length)
 {
+    /* create a new array to copy into to prevent corruption */
     uint8_t my_array[length];
+    /* copy our current array into the new array */
     my_memcpy(src, my_array, length);
+    /* copy the new array into the destination array*/
     return my_memcpy(my_array, dst, length);
 }
 
 uint8_t * my_memcpy(uint8_t * src, uint8_t * dst, size_t length)
 {
+    /* check for null pointers */
+    if(!src || !dst)
+    {
+        return NULL;
+    }
+    /* create a variable to iterate through (for loop) */
     size_t i = 0;
+    /* save the original destination since we'll be changing its value */
     uint8_t* orig_dst = dst;
+    /* loop for how many bytes there are */
     for(i=0;i<length;i++)
     {
+        /* copy the source to the destination */
         *dst++ = *src++;
     }
     return orig_dst;
@@ -20,9 +34,17 @@ uint8_t * my_memcpy(uint8_t * src, uint8_t * dst, size_t length)
 
 uint8_t * my_memset(uint8_t * src, size_t length, uint8_t value)
 {
+    /* check for null pointers */
+    if(!src)
+    {
+        return NULL;
+    }
+    /* create a variable to iterate through (for loop) */
     size_t i = 0;
+    /* loop for how many bytes there are */
     for(i=0;i<length;i++)
     {
+        /* copy the value into the source */
         *src++ = value;
     }
     return src;
@@ -30,23 +52,39 @@ uint8_t * my_memset(uint8_t * src, size_t length, uint8_t value)
 
 uint8_t * my_memzero(uint8_t * src, size_t length)
 {
+    /* just call my_memset with a vlue of 0 */
     return my_memset(src, length, 0); 
 }
 
 uint8_t * my_reverse(uint8_t * src, size_t length)
 {
+    /* check for null pointers */
+    if(!src)
+    {
+        return NULL;
+    }
+    /* create a temporary array */
     uint8_t my_array[length];
+    /* copy the source into the temporary array */
     my_memcpy(src, my_array, length)
-    my_array += length-1;
+    /* create an address that points to the end of the array */
+    uint8_t * my_array_ptr = &my_array[length-1];
+    /* loop for how many bytes there are */
     for(i=0;i<length;i++)
     {
-        *src++ = *my_array--;
+        /* copy the destination of the pointer
+         * of the end of the array into the srouce */
+        *src++ = *my_array_ptr--;
     }
     return src;
 }
 
 void * reserve_words(size_t length)
 {
+    void* start_of_heap = (void*)&__HeapBase;
+    void* end_of_heap = (void*)&__HeapLimit;
+    size_t used_of_heap = (size_t)__HeapBase;
+
 }
 
 uint8_t free_words(void * src)
