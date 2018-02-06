706f9deb2fe6b10f09a7ec085980af15ebb05168
sethm <sethm@xilinx.com>
2018-02-05 18:26:14 -0700

    bugfixes and first compilation through makefile

---
+++ project1/sources.mk
@@ -0,0 +1,12 @@
+SRCS = main.c \
+	  project1.h \
+	  project1.c \
+	  conversion.h \
+	  conversion.c \
+	  memory.h \
+	  memory.c \
+	  data.h \
+	  data.c \
+	  debug.h \
+	  debug.c \
+	  platform.h
