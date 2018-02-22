67a55eff1d577a2c1e1b775005a561121fc0f29e
Seth Miers <sethm@xlnx.xilinx.com>
2018-02-21 19:09:03 -0700

    fixed circbuf bugs

--- project2/include/common/circbuf.h
+++ project2/include/common/circbuf.h
@@ -38,12 +38,13 @@
 
 /*
  * A structure to implement a FIFO buffer
+ * TODO need to understand volatile should it be used here?
  */
 typedef struct {
     volatile BUFFER_TYPE* base;             // The base address of the buffer
     volatile size_t buff_size;              // the total size of the buffer
-    volatile size_t head;                   // Where next byte should be put in the buff
-    volatile size_t tail;                   // Oldest byte in buff
+    volatile BUFFER_TYPE* head;                   // Where next byte should be put in the buff
+    volatile BUFFER_TYPE* tail;                   // Oldest byte in buff
     volatile size_t num_in;                 // Number of bytes in buff
     volatile uint8_t buff_empty_flag:1;     // Flag to show if the buffer is empty
     volatile uint8_t buff_full_flag:1;      // Flag to show if the buffer is full
@@ -85,21 +86,21 @@ typedef enum {
  * is passed in, otherwise a stack overflow may occur.
  *
  *
- * @param[in] CB_t the CB_t object to initialize
+ * @param[in] CB_t* the CB_t object to initialize
  * @param[in] size_t the size of the buffer to initialize
  * @return CB_e This function returns the CB_e typedef to indicate errors
  */
-CB_e CB_init(CB_t circbuff, size_t buffer_size);
+CB_e CB_init(CB_t* circbuff, size_t buffer_size);
 
 /* @brief frees the entire circular buffer
  *
  * this function simply frees the dynamic memory that it allowcated
  * when initializing the function.
  *
- * @param[in] CB_t the CB_t object to destroy
+ * @param[in] CB_t* the CB_t object to destroy
  * @return CB_e This function returns the CB_e typedef to indicate errors
  */
-CB_e CB_destroy(CB_t circbuff);
+CB_e CB_destroy(CB_t* circbuff);
 
 /* @brief adds an item to the circular buffer
  *
@@ -107,32 +108,32 @@ CB_e CB_destroy(CB_t circbuff);
  * increment the head, and update all the flags
  *
  *
- * @param[in] CB_t the CB_t object to operate on
+ * @param[in] CB_t* the CB_t object to operate on
  * @param[in] BUFFER_TYPE the object to add into the buffer
  * @return CB_e This function returns the CB_e typedef to indicate errors
  */
-CB_e CB_buffer_add_item(CB_t circbuff, BUFFER_TYPE data);
+CB_e CB_buffer_add_item(CB_t* circbuff, BUFFER_TYPE data);
 
 /* @brief removes an item from the circular buffer
  *
  * removes an item from the circular buffer by
  * incrementing the tail an decrementing the num_in
  *
- * @param[in] CB_t the CB_t object to operate on
+ * @param[in] CB_t* the CB_t object to operate on
  * @param[out] BUFFER_TYPE* put the data removed into this pointer
  * @return CB_e This function returns the CB_e typedef to indicate errors
  */
-CB_e CB_buffer_remove_item(CB_t circbuff, BUFFER_TYPE* data);
+CB_e CB_buffer_remove_item(CB_t* circbuff, BUFFER_TYPE* data);
 
 /* @brief checks to see if the buffer is full
  *
  * simply checks the full flag of the buffer to see
  * if the head ever passed the tail
  *
- * @param[in] CB_t the CB_t object to operate on
+ * @param[in] CB_t# the CB_t object to operate on
  * @return CB_e This function returns the CB_e typedef to indicate errors
  */
-CB_e CB_is_full(CB_t circbuff);
+CB_e CB_is_full(CB_t* circbuff);
 
 /* @brief checks to see if the buffer is empty
  *
@@ -141,21 +142,21 @@ CB_e CB_is_full(CB_t circbuff);
  * there is no data in the buffer
  *
  *
- * @param[in] CB_t the CB_t object to operate on
+ * @param[in] CB_t* the CB_t object to operate on
  * @return CB_e This function returns the CB_e typedef to indicate errors
  */
-CB_e CB_is_empty(CB_t circbuff);
+CB_e CB_is_empty(CB_t* circbuff);
 
 /* @brief returns the value in the buffer at a position back from the head
  *
  * peeking at the 0th value will just return the last value
  * that was put into the buffer.
  *
- * @param[in] CB_t the CB_t object to operate on
+ * @param[in] CB_t* the CB_t object to operate on
  * @param[in] size_t the index away from the head
  * @param[out] BUFFER_TYPE* put the data peeked at into this pointer
  * @return CB_e This function returns the CB_e typedef to indicate errors
  */
-CB_e CB_peek(CB_t circbuff, size_t position, BUFFER_TYPE* data);
+CB_e CB_peek(CB_t* circbuff, size_t position, BUFFER_TYPE* data);
 
 #endif /*__CIRCBUF_H__*/
