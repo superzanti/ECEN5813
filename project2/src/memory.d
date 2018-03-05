f2b36b2c187e8d61bd7417302c633d66d724f30f
Seth Miers <semi1529@colorado.edu>
2018-03-04 23:26:36 -0700

    i'm an idiot that likes to add .o files tot he git

--- project2/src/memory.d
+++
@@ -1,9 +0,0 @@
-memory.o: memory.c /usr/include/stdc-predef.h ../include/common/memory.h \
- /usr/lib/gcc/x86_64-linux-gnu/5/include/stdint.h /usr/include/stdint.h \
- /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
- /usr/include/x86_64-linux-gnu/bits/wordsize.h \
- /usr/include/x86_64-linux-gnu/gnu/stubs.h \
- /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
- /usr/include/x86_64-linux-gnu/bits/wchar.h \
- /usr/lib/gcc/x86_64-linux-gnu/5/include/stddef.h /usr/include/stdlib.h \
- /usr/include/x86_64-linux-gnu/bits/stdlib-float.h
